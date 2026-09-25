e113e65062d6560081f6d6f395ba5a34eb53e3e6
Jake Santiago Russell <m_le@fanshaweonline.ca>
2026-09-25 17:32:44 -0400

    Added JS for drag and drop

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const temp = document.querySelector("template");
 const searchBar = document.querySelector("#task");
 
 let taskIndex = 0;
+let currentItem = null;
 
 
 function addItem() {
@@ -15,6 +16,7 @@ function addItem() {
     let taskId = `${taskName}-${taskIndex}`;
 
     let listItem = temp.content.cloneNode(true);
+
     listItem.querySelector("input").id = taskId;
     listItem.querySelector("label").htmlFor = taskId;
     listItem.querySelector("label").textContent = taskName;
@@ -38,9 +40,41 @@ function moveItem(e,index) {
     }
 }
 
+function startDrag(e) {
+    if (e.target.tagName === "LI") {
+        currentItem = e.target;
+        console.log(currentItem)
+        //e.dataTransfer.setData('text', e.target.firstElementChild.id);
+        //console.log(e.dataTransfer.getData('text'));
+    }
+}
+
+function swapPos(e, list) {
+    //e.preventDefault();
+
+    if (e.target.tagName === "LI") {
+        let nextItem = e.target;
+        console.log(list.children.indexOf(nextItem));
+        e.stopPropagation();
+        /*if(list.children.indexOf(nextItem)){
+            nextItem.before(currentItem);
+        } else {
+            nextItem.after(currentItem);
+        }*/
+    }
+}
+
 lists.forEach((list, index) => {
     list.addEventListener("animationend", (e) => {
         moveItem(e,index);
+    })
+
+    list.addEventListener("dragstart", (e) => {
+        startDrag(e);
+    })
+
+    list.addEventListener("dragenter", (e) => {
+        swapPos(e, list);
     })
 });
 
